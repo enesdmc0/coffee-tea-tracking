e722876b39bb48a8c8f1bdb86167bfd33d9b165f
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2024-09-05 15:00:28 +0300

    installation completed

---
+++ app/+not-found.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+
+export default function NotFoundScreen() {
+    return (
+        <View>
+            <Text>Page Not Found</Text>
+        </View>
+    );
+}
+
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+
+export default function Home() {
+    return (
+        <View className="bg-red-400">
+            <Text>Enes Demirci</Text>
+        </View>
+    );
+}
+
